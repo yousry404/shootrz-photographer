8341da8aab37a76348499e6ab993ece747e32e90
AbdelrhmanYousry <eng_abdelrhman93@yahoo.com>
2020-01-21 18:20:41 +0200

    update api url

--- src/services/api.js
+++ src/services/api.js
@@ -1,2 +1,2 @@
-export const baseUrl = "http://localhost:5001/api"
-// export const baseUrl = "https://shootrz-api.herokuapp.com/api"
+// export const baseUrl = "http://localhost:5001/api"
+export const baseUrl = "https://shootrz-api.herokuapp.com/api"
